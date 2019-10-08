ad7d351dee2e2f84767fb38f36d15bee491e011c
Amanda <amandamarienelson2@gmail.com>
2019-10-07 23:04:45 -0500

    medium

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
-import Writing from '../sections/Writing';
+// import Writing from '../sections/Writing';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Facts from '../sections/Facts';
